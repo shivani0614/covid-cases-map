300e5a001ffc55a9479b53c4e3d158d6e0da04ff
shivani0614 <shivanialeti14@gmail.com>
2024-01-14 14:56:48 -0700

    formating the number of  covid confirmed cases number

--- src/tasks/LoadCountries.js
+++ src/tasks/LoadCountries.js
@@ -19,28 +19,31 @@ class LoadCountries {
     // setState(features);
   };
   #processCovidData = (covidCountries) => {
-     console.log("cc",covidCountries);
+    //  console.log("cc",covidCountries);
      
     for (let i = 0; i < data.features.length; i++) {
       const country = data.features[i];
-      console.log("map", country);
+    //   console.log("map", country);
       const covidCountry = covidCountries.find(
         (covidCountry) => covidCountry.ISO3 === country.properties.ISO_A3
       );
-      console.log("covidCountry", covidCountry);
+      
       country.properties.confirmed = 0;
       country.properties.confirmedText = "0";
-     console.log("covidCountry",covidCountry);
+    //  console.log("covidCountry",covidCountry);
 
       if (covidCountry != null) {
         const confirmed = Number(covidCountry.Confirmed);
-        console.log("number",confirmed)
+        // console.log("number",confirmed)
         country.properties.confirmed = confirmed;
-        country.properties.confirmedText = confirmed; 
+        country.properties.confirmedText = this.#formatNumberWithCommas(confirmed); 
       }
     }
     this.setState(data.features);
   };
+  #formatNumberWithCommas=(number)=>{
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 }
 
 export default LoadCountries;
